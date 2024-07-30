9a2d3925efd48d2a740db60ebde54017cde4fedf
kennyesidganadero <kennyesidganadero@gmail.com>
2024-07-29 22:04:55 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ function App() {
     return (
       <div className='divPadre'>
         <div className='divHijo'>
-          <img src="loading-logo-2.gif" alt="v1.0.0" width='150px' height='auto' />
+          <img src="loading-logo-2.gif" alt="v1.0.1" width='150px' height='auto' />
         </div>
       </div>
     )
