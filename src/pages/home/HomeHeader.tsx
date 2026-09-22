42888ad6bbe183e7369b8f43263d2537b28c771d
Yesid <kennyesid@gmail.com>
2026-09-21 23:56:42 -0400

    update

--- src/pages/home/HomeHeader.tsx
+++ src/pages/home/HomeHeader.tsx
@@ -65,7 +65,7 @@ const HomeHeader: React.FC<HomeHeaderProps> = ({
           <img onClick={redirectPageLinkedin} src="icons8-linkedin-4.svg" />
           <img onClick={redirectPageGitHub} src="icon-github.svg" />
           <img onClick={redirectPageInstagram} src="icon-instagram.svg" />
-          <img onClick={redirectPageWhatsapp} src="icon-whatsapp.svg" />
+          <img onClick={redirectPageWhatsapp} src="icon-whatsapp-4.svg" />
         </div>
       </div>
       <div className="home-content">
